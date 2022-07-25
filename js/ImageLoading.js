d4f01efecad7f6202971d4a9a936a51af5212f38
Patrick McKeown <j.p.mckeown@icloud.com>
2022-07-25 12:15:38 +0100

    swapped playerPic to end of imageList, nothing loads

--- js/ImageLoading.js
+++ js/ImageLoading.js
@@ -5,6 +5,7 @@ var picsToLoad = 0; // set automatically based on imageList in loadImages()
 
 function countLoadedImagesAndLaunchIfReady() {
 	picsToLoad--;
+console.log("pics to load:", picsToLoad)
 	if(picsToLoad == 0) {
 		imageLoadingDoneSoStartGame();
 	}
@@ -22,7 +23,6 @@ function loadImageForTileCode(tileCode, fileName) {
 
 function loadImages() {
 	var imageList = [
-		{varName: playerPic, theFile: "player_hat.png"},
 		{tileType: TILE_FIELD, theFile: "tile_grass.png"},
     {tileType: TILE_WALL, theFile: "tile_wall.png"},		
     {tileType: TILE_GOAL, theFile: "tile_goal.png"},
@@ -30,16 +30,19 @@ function loadImages() {
     {tileType: TILE_PEN_RED, theFile: "tile_pen_red.png"},
     {tileType: TILE_TREE, theFile: "tile_tree.png"},
 		{tileType: TILE_FLAG_LEFT, theFile: "tile_flag_left.png"},
-		{tileType: TILE_FLAG_RIGHT, theFile: "tile_flag_right.png"}
+		{tileType: TILE_FLAG_RIGHT, theFile: "tile_flag_right.png"},
+    {varName: playerPic, theFile: "player_hat.png"}
 	];
 	picsToLoad = imageList.length;
 console.log('images to load =', imageList.length);
 console.log(imageList);
+console.log("varName", imageList[0].varName);
 	for(var i=0; i<imageList.length; i++) {
     // if list item has varName, use it
     if(imageList[i].varName != undefined) {
+console.log("varName", imageList[i].varName);
       beginLoadingImage(imageList[i].varName, imageList[i].theFile);
-console.log("varname", imageList[i].varName);
+
     } else {
       loadImageForTileCode(imageList[i].tileType, imageList[i].theFile);
     }
