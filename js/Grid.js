a1c6d5bb32ac9307b12be0de95027b1c8c03f5be
Patrick McKeown <j.p.mckeown@icloud.com>
2022-08-06 07:14:35 +0100

    add Level zero for testing asset integration

--- js/Grid.js
+++ js/Grid.js
@@ -16,8 +16,26 @@ const TILE_UNSORT = 11;
 var areaGrid = [];
 var saveGrid = [];
 
-// no level 0
-const levelNames =[null, "Call and send", "Avoid the lakes", "Rogue appears", "Out of the woods"];
+// level 0 scratchpad for asset integration testing
+const levelNames =["Testing", "Call and send", "Avoid the lakes", "Rogue appears", "Out of the woods", "Name of Level 5", "Name of Level 6", "Name of Level 7", "Name of Level 8", "Name of Level 9"];
+
+const level_0 = [
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  3, 3, 3, 3, 3, 3, 3, 3, 3, 1, 8, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3,
+];
 
 const level_1 = [
   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
@@ -182,4 +200,4 @@ const level_9 = [
 ];
 
 // level number 0 bad for UI, and shifting +1 everywhere was awkward  
-var levelList = [null, level_1, level_2, level_3, level_4, level_5, level_6, level_7, level_8, level_9];
+var levelList = [level_0, level_1, level_2, level_3, level_4, level_5, level_6, level_7, level_8, level_9];
--- js/Input.js
+++ js/Input.js
@@ -82,7 +82,7 @@ function keyState(key) {
         break;
       }
       if(editMode) {
-        if(key >= KEY_NUM_1 && key <= KEY_NUM_9) {
+        if(key >= KEY_NUM_0 && key <= KEY_NUM_9) {
           currentLevel = key - KEY_NUM_0; // 1 on keyb is code 49
           // console.log("Level number now =", currentLevel);
           loadLevel(currentLevel);
