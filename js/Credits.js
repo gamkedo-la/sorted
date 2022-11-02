5cc672b29e961266607dc9e81dbdd8fea6303e7a
Gamkedo <chris@gamkedo.com>
2022-11-01 21:12:59 -0400

    Credits updated

--- js/Credits.js
+++ js/Credits.js
@@ -1,9 +1,10 @@
 const creditsText = [
-"Patrick McKeown: Project lead, core gameplay, custom level editor, collisions, slow tiles, UI, main tile art including grass and pens, call effects, tutorial, obstacles and characters, level design, scoring, mobile/tablet support, menus, automated tests, game event feedback effects, many bug fixes, additional sounds",
+"Patrick McKeown: Project lead, core gameplay, custom level editor, collisions, lorry/truck ending sequence, slow tiles, UI, main tile art including grass and pens, call effects, tutorial, obstacles and characters, level design, scoring, mobile/tablet support, menus, automated tests, game event feedback effects, many bug fixes, additional sounds (includng barks, waterfowl)",
 "Christer \"McFunkypants\" Kaitila: Animated menu backgrounds, decals (flowers, grass, hoof prints), ground decoration, call/send tutorial tooltips, baa and woof sounds with ambient and pen hookups, tile art (forest, lake, bend, conveyor), main menu drop shadow, victory music for end of level",
 "H Trayford: Wrap screen fix, debug level skip, max score calculation, credits scroll",
-"Vince McKeown: Hat art",
-"Gonzalo Delgado: Rogue dog sprite",
+"Vince McKeown: Lorry/truck art, hat art, level time display",
+"Gonzalo Delgado: Rogue dog sprite, UI button graphic with integration",
+"FightEXP: Goal tile",
   "Tim Waskett: Explained algorithm for sheep roaming",
 "Chris DeLeon: Sheep face, menu return sound, muddy tile",
   'Nicholas Polchies: Canvas scaling (for phone screens) code from HomeTeam game "Accidental Personal Confusion 5"',
